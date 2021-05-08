ea28b4e30a5b4a4fac5707af984ff170598922bf
Farid Fadil <33313844+faridfadil@users.noreply.github.com>
2021-05-08 20:13:08 +1000

    First Commit

---
+++ rotation_cipher.py
@@ -0,0 +1,65 @@
+#ascii encryptor
+print("Ignore me :)")
+
+from tkinter import *
+
+#tkinter main window setup
+root = Tk()
+root.title("Rotation Cipher Program")
+
+#FUNCTIONS
+def encode():
+        code_text.delete("1.0", "end-1c")
+        shift_amount = int(shift_entry.get())
+        text = message_entry.get("1.0", "end-1c")
+        code = ""
+        
+        for x in text:
+                encoded_ascii = ord(x) + shift_amount
+                code += chr(encoded_ascii)
+        code_text.insert("1.0", code)
+def decode():
+        code_text.delete("1.0", "end-1c")
+        shift_amount = int(shift_entry.get())
+        text = message_entry.get("1.0", "end-1c")
+        code = ""
+        for x in text:
+                try:
+                        decoded_ascii = ord(x) - shift_amount
+                        decoded_word = chr(decoded_ascii)
+                        code += decoded_word
+                except:
+                        pass
+        code_text.insert("1.0", code)
+
+
+shift_label = Label(root, text="Shift Key")
+shift_label.grid(row=0, column=0)
+
+shift_entry = Entry(root)
+shift_entry.grid(row=0, column=1, sticky=W)
+
+message_label = Label(root, text="Message")
+message_label.grid(row=1, column=0, sticky=N)
+
+message_entry = Text(root, height=10, width=50)
+message_entry.grid(row=1, column=1)
+
+code_label = Label(root, text="Coded Message")
+code_label.grid(row=2, column=0, sticky=N)
+
+code_text = Text(root, height=10, width=50)
+code_text.grid(row=2, column=1)
+
+encrypt_button = Button(root, text="Encrypt Message", command=encode)
+decrypt_button = Button(root, text="Decrypt Message", command=decode)
+
+encrypt_button.grid(row=3, column=0)
+decrypt_button.grid(row=3, column=1, sticky=W)
+
+#message_text = message_entry.get("1.0", "end-1c")
+
+
+root.mainloop()
+
+
