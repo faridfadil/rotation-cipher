cec13944899acb4e4baded96fe2a427c4f37841d
Farid Fadil <33313844+faridfadil@users.noreply.github.com>
2021-08-08 10:50:19 +1000

    Commented the code

--- rotation_cipher.py
+++ rotation_cipher.py
@@ -9,30 +9,49 @@ root.title("Rotation Cipher Program")
 
 #FUNCTIONS
 def encode():
+        #clear text field
         code_text.delete("1.0", "end-1c")
+        #get the shift value from the entry and cast it into an int type
         shift_amount = int(shift_entry.get())
+        #get the text entered into the message field
         text = message_entry.get("1.0", "end-1c")
+        #empty code string for encoded message to be later appended to. 
         code = ""
         
-        for x in text:
-                encoded_ascii = ord(x) + shift_amount
+        #for every letter in the text field
+        for letter in text:
+                #convert the letter into an ASCII value and add the shift_amount value to it
+                encoded_ascii = ord(letter) + shift_amount
+                #convert the shifted ASCII value into a character and add it to the empty string
                 code += chr(encoded_ascii)
+        #print out the value onto the coded message text entry. 
         code_text.insert("1.0", code)
 def decode():
+        #clear text field
         code_text.delete("1.0", "end-1c")
+        #get the shift value from the entry and cast it into an int type
         shift_amount = int(shift_entry.get())
+        #get the text entered into the message field
         text = message_entry.get("1.0", "end-1c")
+        #empty code string for encoded message to be later appended to. 
         code = ""
-        for x in text:
+
+        #for every letter in the text field
+        for letter in text:
                 try:
-                        decoded_ascii = ord(x) - shift_amount
+                        #convert the letter into an ASCII value and subtract the shift_amount value to it
+                        decoded_ascii = ord(letter) - shift_amount
+                        #convert the shifted ASCII value into a character. 
                         decoded_word = chr(decoded_ascii)
+                        #add decoded character to the empty string
                         code += decoded_word
                 except:
                         pass
+        #print out the value onto the coded message text entry
         code_text.insert("1.0", code)
 
 
+#TKINTER GUI
 shift_label = Label(root, text="Shift Key")
 shift_label.grid(row=0, column=0)
 
